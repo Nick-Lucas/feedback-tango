5a78e2e686b48cb72b908586a6ba37627db04205
Nick Lucas <me@nicklucas.co.uk>
2025-09-18 16:45:40 +0100

    Disable some buggy eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -35,8 +35,12 @@ export default [
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
-      // Just buggy as hell:
+      // These error on perfectly valid code until restart:
       '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      //
     },
   },
   {
