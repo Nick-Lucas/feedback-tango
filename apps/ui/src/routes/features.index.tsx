b6874237c84ef4661b1d80848d298390e2699096
Nick Lucas <me@nicklucas.co.uk>
2025-10-03 20:45:36 +0100

    Niceer select a feature copy

--- apps/ui/src/routes/features.index.tsx
+++ apps/ui/src/routes/features.index.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Ellipsis } from 'lucide-react'
 
 export const Route = createFileRoute('/features/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Select a feature to explore your feedback!</div>
+  return (
+    <div className="flex flex-col items-center justify-center h-full text-center">
+      <Ellipsis className="w-24 h-24 text-gray-500 mb-4" />
+      <p className="text-gray-500">Select a feature to explore its feedback</p>
+    </div>
+  )
 }
