12d3bb8a367dfd70296530b44fc39814ffeaab22
Nick Lucas <me@nicklucas.co.uk>
2025-10-07 15:23:48 +0100

    Rename app

--- apps/ui/src/routes/__root.tsx
+++ apps/ui/src/routes/__root.tsx
@@ -42,7 +42,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Project Tango',
+        title: 'Feedback Tango',
       },
     ],
     links: [
