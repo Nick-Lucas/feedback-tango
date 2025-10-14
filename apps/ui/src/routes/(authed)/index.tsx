e1ac6cb9a321d6ba53e2721183ec115e6b7e8309
Nick Lucas <me@nicklucas.co.uk>
2025-10-14 10:56:38 +0100

    Add landing page dummy

--- apps/ui/src/routes/(authed)/index.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute, Navigate } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(authed)/')({
-  component: () => <Navigate to="/projects" replace />,
-})
