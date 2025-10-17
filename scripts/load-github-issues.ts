2c54c6e58651c510f6f936d5a92145036d475e4e
Nick Lucas <me@nicklucas.co.uk>
2025-10-17 22:47:14 +0100

    Fix script

--- scripts/load-github-issues.ts
+++ scripts/load-github-issues.ts
@@ -8,8 +8,16 @@ import path from 'path'
 
 import { createDb } from '../libs/db/src/db/db.ts'
 import { RawFeedbacks } from '../libs/db/src/db/schema.ts'
+import { config } from 'dotenv'
 
-const issuesPath = path.join(__dirname, '../issues.json')
+config({
+  path: '.env',
+})
+
+const issuesPath = path.join(
+  import.meta.dirname,
+  '../.backups/trpc-issues.json'
+)
 const issuesJson = fs.readFileSync(issuesPath, 'utf-8')
 const issues = JSON.parse(issuesJson) as Array<{
   number: number
@@ -24,18 +32,23 @@ const issues = JSON.parse(issuesJson) as Array<{
 
 const db = createDb()
 
-const PROJECT_ID = ''
+const PROJECT_ID = '0199dfc9-d041-73b5-8c1b-df043cf368b6'
 
 async function loadIssues() {
-  for (const issue of issues) {
-    console.log(`Loading issue #${issue.number}: ${issue.title}`)
-    await db.insert(RawFeedbacks).values({
-      content: `# ${issue.title}\n\n${issue.body}`,
-      projectId: PROJECT_ID,
-      email: issue.author.login + '~users.github.com',
-      // TODO: need a way to track external IDs like the github issue number against an integration
-    })
-  }
+  await db.transaction(async (tx) => {
+    for (const issue of issues) {
+      console.log(`Loading issue #${issue.number}: ${issue.title}`)
+      await tx.insert(RawFeedbacks).values({
+        content: `# ${issue.title}\n\n${issue.body}`,
+        projectId: PROJECT_ID,
+        email: issue.author.login + '~users.github.com',
+        // TODO: need a way to track external IDs like the github issue number against an integration
+      })
+    }
+  })
 }
 
 await loadIssues()
+
+await db.$client.end()
+console.log('Done loading issues.')
